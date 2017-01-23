23919d8dc3166411800577a51a2742d35172eadc
Vinay Khobragade <vinaykhobragade99@gmail.com>
2017-01-23 17:44:01 +0530

    String sum in c

---
+++ string-sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[1000];
+    scanf("%s", &str);
+    int len=strlen(str);
+    int sum=0;
+    
+    while(len--) {
+
+        sum += (str[len]-96);
+        i++;
+        
+    }
+    printf("%d", sum);
+    return 0;
+}
